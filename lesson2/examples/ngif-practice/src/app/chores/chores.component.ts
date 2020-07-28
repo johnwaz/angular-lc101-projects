5d60a5355c98f194a1f94630ffe307471655a838
John Wasielewicz <wazjohn@yahoo.com>
2020-07-28 16:22:01 -0500

    Completed lesson2 examples of ngFor and ngIf.

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = ['Empty dishwasher'];
+   finishedChores = ['Complete LaunchCode prep work', 'Buy groceries', 'Take out trash', 'Walk dog', 'Pay bills'];
 
-   trophyImage = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
+   trophyImage = 'https://findicons.com/files/icons/2431/business_clean/256/trophy.png';
 
    constructor() { }
 
